b220f537c2a4fc65950a9cf63d946ce0acb8bb34
Asanga <asanga.nicholas@yahoo.com>
2019-10-25 10:24:36 +0200

    added response server

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.listen(port, ()=> console.log("sever running on port", port))
+
+app.get('/',(req, res, next)=>{
+    console.log('get a request on /')
+    res.status(200)
+    res.send('hello world')
+})
