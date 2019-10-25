3da0dd64de9a137791330fb1d41571ef640fc9ab
Asanga <asanga.nicholas@yahoo.com>
2019-10-25 11:52:10 +0200

    completed the server

--- stream/router.js
+++ stream/router.js
@@ -6,11 +6,17 @@ const Sse = require('json-sse')
 const router = new Router()
 const stream = new Sse()
 
-router.get('/stream', (req, res) =>{
+router.get('/stream', async (req, res) =>{
     console.log('got a request on /stream')
 
-    res.status(200)
-    res.send('it works')
+    // res.status(200)
+    // res.send('it works')
+    const messages = await Chatroom.findAll()
+    const data = JSON.stringify(messages)
+    console.log("stringified message:", data)
+
+    stream.updateInit(data) // put data in the stream 
+    stream.init(req,res) // important
 })
 
 router.post('/message', async (req, res) =>{
@@ -19,9 +25,20 @@ router.post('/message', async (req, res) =>{
     const entity = await Chatroom.create({
         message: message
     })
+
+
+
+    const messages = await Chatroom.findAll()
+    const data = JSON.stringify(messages)
+    // console.log("stringified message:", data)
+    stream.send(data)
+
+
+
     res.status(201)
     res.send('Thanks for your message')
 })
+
 
 
 module.exports = router
